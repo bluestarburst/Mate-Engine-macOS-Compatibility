e684ab67b43c420a71db49bdd6f11e09b3c26065
Bryant Hargreaves <hargreaves.bryant@gmail.com>
2026-01-02 01:06:29 -0600

    windows

--- Assets/MATE ENGINE - System Tray/SystemTray/TrayIcon.cs
+++ Assets/MATE ENGINE - System Tray/SystemTray/TrayIcon.cs
@@ -261,5 +261,12 @@ namespace Utils
             Debug.Log("Cleaned up resources for System Tray Icon");
 #endif
         }
+
+        /// <summary>Dispose of the System Tray Icon and cleanup resources</summary>
+        public static void Dispose()
+        {
+            CleanupResources();
+            _init = false;
+        }
     }
 }
